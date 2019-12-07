d0df259640da008b5bdbf6ac9e963ce755e19721
Jonathan Lawerh <lawerhjonathan@gmail.com>
2019-12-07 14:51:26 +0000

    Added CircleCI Configuration

--- app/src/androidTest/java/com/creativeapps/partyapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/creativeapps/partyapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.creativeapps.partyapp;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
