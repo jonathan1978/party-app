3c44e3d9d90f3d161535ae2227bb8a6344d671d8
Jonathan Lawerh <lawerhjonathan@gmail.com>
2019-12-08 08:33:32 +0000

    Setup Project

---
+++ app/src/main/java/com/creativeapps/partyapp/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.creativeapps.partyapp.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.creativeapps.partyapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
