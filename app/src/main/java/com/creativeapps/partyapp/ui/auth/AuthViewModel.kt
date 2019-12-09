8a482b4045cbec0daebbf775f82be7af9f0cd353
Jonathan Lawerh <lawerhjonathan@gmail.com>
2019-12-09 22:38:03 +0000

    Added Home and Profile View

--- app/src/main/java/com/creativeapps/partyapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/creativeapps/partyapp/ui/auth/AuthViewModel.kt
@@ -1,5 +1,6 @@
 package com.creativeapps.partyapp.ui.auth
 
+import android.content.Intent
 import android.view.View
 import androidx.lifecycle.ViewModel
 import com.creativeapps.partyapp.data.repositories.UserRepository
@@ -10,8 +11,11 @@ import com.creativeapps.partyapp.util.NoInternetException
 class AuthViewModel(
         private val repository: UserRepository
 ) : ViewModel() {
+
+    var name: String? = null
     var email: String? = null
     var password: String? = null
+    var passwordConfirm: String? = null
 
     var authListener: AuthListener? = null
 
@@ -38,6 +42,58 @@ class AuthViewModel(
                 authListener?.onFailure(e.message!!)
             }
 
+        }
+    }
+
+    fun onLogin(view: View){
+        Intent(view.context, LoginActivity::class.java).also {
+            view.context.startActivity(it)
+        }
+    }
+
+    fun onSignup(view: View){
+        Intent(view.context, SignupActivity::class.java).also {
+            view.context.startActivity(it)
+        }
+    }
+
+    fun onSignupButtonClick(view: View){
+        authListener?.onStarted()
+
+        if (name.isNullOrEmpty()){
+            authListener?.onFailure("Name is Required")
+            return
+        }
+
+        if (email.isNullOrEmpty()){
+            authListener?.onFailure("Email is Required")
+            return
+        }
+
+        if (password.isNullOrEmpty()){
+            authListener?.onFailure("Password is Required")
+            return
+        }
+
+        if (password != passwordConfirm){
+            authListener?.onFailure("Password did not match")
+            return
+        }
+
+        Coroutines.main {
+            try {
+                val authResponse = repository.userSignup(name!!, email!!, password!!)
+                authResponse.user?.let {
+                    authListener?.onSuccess(it)
+                    repository.saveUser(it)
+                    return@main
+                }
+                authListener?.onFailure(authResponse.message!!)
+            }catch (e: ApiException){
+                authListener?.onFailure(e.message!!)
+            }catch (e: NoInternetException){
+                authListener?.onFailure(e.message!!)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/creativeapps/partyapp/ui/home/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.creativeapps.partyapp.ui.home.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+}
